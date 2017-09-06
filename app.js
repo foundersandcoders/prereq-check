d28e66dde9e30f19cd5bbb170795293e89a23ffc
Bart Bucknill <bartbucknill@hotmail.com>
2017-09-06 11:21:48 +0100

    #9 Change router pattern

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var router = require('./routes/index');
 
 var app = express();
 
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
