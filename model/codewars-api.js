c748084bd136638ef7601d988bf897c89489ace8
Aisha <astroaisha@gmail.com>
2017-09-07 12:05:26 +0000

    #2 refactored codewars api to use promises with request-promise-native

--- model/codewars-api.js
+++ model/codewars-api.js
@@ -1,25 +1,55 @@
 const request = require('request');
+const rp = require('request-promise-native')
 
-const getKyu = (username, cb) => {
-    const apiUrl = 'https://www.codewars.com/api/v1/users/' + username;
-    request(apiUrl, (error, response, body) => {
-        if (error) {
-            cb(error);
-        } else if (!error && response.statusCode === 200) {
+// const getKyu = (username, cb) => {
+//     const apiUrl = 'https://www.codewars.com/api/v1/users/' + username;
+//     request(apiUrl, (error, response, body) => {
+//         if (error) {
+//             cb(error);
+//         } else if (!error && response.statusCode === 200) {
+//             const codewarsSuccess = {};
+//             const codewarsRank = JSON.parse(body).ranks.languages.javascript.rank;
+//             codewarsSuccess.success= true;
+//             codewarsSuccess.rank = Math.abs(codewarsRank);
+//             cb(null, codewarsSuccess);
+//         } else {
+//             const codewarsError = {};
+//             codewarsError.success = false;
+//             codewarsError.statusCode = response.statusCode;
+//             codewarsError.body = JSON.parse(body);
+//             cb(null, codewarsError);
+//         }
+//     });
+// };
+
+const options = {
+    uri: 'https://www.codewars.com/api/v1/users/astroash',
+    json: true, // Automatically parses the JSON string in the response
+    resolveWithFullResponse: true
+};
+
+const getKyu = (response, body) => {
+        if (response.statusCode === 200) {
             const codewarsSuccess = {};
-            const codewarsRank = JSON.parse(body).ranks.languages.javascript.rank;
+            const codewarsRank = response.body.ranks.languages.javascript.rank;
             codewarsSuccess.success= true;
             codewarsSuccess.rank = Math.abs(codewarsRank);
-            cb(null, codewarsSuccess);
+            console.log(codewarsSuccess)
+            return codewarsSuccess;
         } else {
             const codewarsError = {};
             codewarsError.success = false;
             codewarsError.statusCode = response.statusCode;
-            codewarsError.body = JSON.parse(body);
-            cb(null, codewarsError);
+            codewarsError.body = body;
+            return codewarsError;
         }
-    });
 };
+
+rp(options)
+    .then(getKyu)
+    .catch(function (err) { 
+        console.log(err);
+    });
 
 //getKyu('astroashf', console.log);
 
--- routes/report.js
+++ routes/report.js
@@ -3,9 +3,9 @@ const getKyu = require("../model/codewars-api");
 const displayReport = (req, res) => {
     const summaryObject = {};
     getKyu("astroash", (error, apiResponse) => {
-        summaryObject.codewarsKyu = apiResponse.success && apiResponse.rank >= 5;
+        summaryObject.codewarsKyu = apiResponse.success && apiResponse.rank <= 5;
         res.render('report', summaryObject);
-        console.log(summaryObject)
+        console.log(summaryObject);
     });
 };
 
