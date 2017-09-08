27a277bbc5ed8b779c6c389482e3ba8cd63ad6e8
astroash <astroaisha@gmail.com>
2017-09-08 14:50:37 +0100

    #4 Create functions to validate all fcc sections with tests:zap:

--- model/freecodecamp-crawl.js
+++ model/freecodecamp-crawl.js
@@ -4,14 +4,40 @@ const rp = require('request-promise-native');
 const fccArrays = require('./freecodecamp-arrays');
 
 const htmlCssValidator = (htmlString) => {
-  for (i = 0; i < fccArrays.htmlCss.length; i++) {
+  for (let i = 0; i < fccArrays.htmlCss.length; i++) {
     if (htmlString.indexOf(fccArrays.htmlCss[i]) === -1) {
       return false;
-      break;
     }
   }
   return true;
-}
+};
+
+const basicJavaScriptValidator = (htmlString) => {
+  for (let i = 0; i < fccArrays.basicJavaScript.length; i++) {
+    if (htmlString.indexOf(fccArrays.basicJavaScript[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const oOFunctionalProgrammingValidator = (htmlString) => {
+  for (let i = 0; i < fccArrays.oOFunctionalProgramming.length; i++) {
+    if (htmlString.indexOf(fccArrays.oOFunctionalProgramming[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const basicScriptingValidator = (htmlString) => {
+  for (let i = 0; i < fccArrays.basicScripting.length; i++) {
+    if (htmlString.indexOf(fccArrays.basicScripting[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
 
 rp('https://www.freecodecamp.org/fcce3abbd74-b40e-4e5d-96a8-c7e1992dcfe1')
   .then()
@@ -21,4 +47,7 @@ rp('https://www.freecodecamp.org/fcce3abbd74-b40e-4e5d-96a8-c7e1992dcfe1')
 
 module.exports = {
   htmlCssValidator,
+  basicJavaScriptValidator,
+  oOFunctionalProgrammingValidator,
+  basicScriptingValidator,
 }
--- tests/freecodecamp.tests.js
+++ tests/freecodecamp.tests.js
@@ -8,5 +8,21 @@ tape('FCC Validation on HTML Page Crawls', (t) => {
   t.ok(actual, 'Returns true when HTML/CSS section is done on dummy success data');
   actual = fccFunctions.htmlCssValidator(fccIncompleteHtmlString);
   t.notok(actual, 'Returns false when HTML/CSS section is done on dummy fail data');
+
+  actual = fccFunctions.basicJavaScriptValidator(fccCompleteHtmlString);
+  t.ok(actual, 'Returns true when Basic JavaScript section is done on dummy success data');
+  actual = fccFunctions.basicJavaScriptValidator(fccIncompleteHtmlString);
+  t.notok(actual, 'Returns false when Basic JavaScript section is done on dummy fail data');
+
+
+  actual = fccFunctions.oOFunctionalProgrammingValidator(fccCompleteHtmlString);
+  t.ok(actual, 'Returns true when Object Oriented & Functional Programming section is done on dummy success data');
+  actual = fccFunctions.oOFunctionalProgrammingValidator(fccIncompleteHtmlString);
+  t.notok(actual, 'Returns false when Object Oriented & Functional Programming section is done on dummy fail data');
+
+  actual = fccFunctions.basicScriptingValidator(fccCompleteHtmlString);
+  t.ok(actual, 'Returns true when Basic Algorithm Scripting section is done on dummy success data');
+  actual = fccFunctions.basicScriptingValidator(fccIncompleteHtmlString);
+  t.notok(actual, 'Returns false when Basic Algorithm Scripting section is done on dummy fail data');
   t.end();
 });
