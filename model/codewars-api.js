248f59edca56384092af1b5876846dc17c661be5
Bart <bartbucknill@hotmail.com>
2017-09-08 13:06:27 +0000

    #2 Uses nock to mock api requst for testing

--- model/codewars-api.js
+++ model/codewars-api.js
@@ -2,17 +2,18 @@ const request = require('request');
 const rp = require('request-promise-native')
 
 const options = {
-  uri: 'https://www.codewars.com/api/v1/users/astroash',
+  uri: 'https://www.codewars.com/api/v1/users/',
   json: true, // Automatically parses the JSON string in the response
-  resolveWithFullResponse: true,
 };
 
-const getKyu = (response) => {
-  const codewarsRank = response.body.ranks.languages.javascript.rank;
+const getKyu = (body) => {
+  console.log(JSON.stringify(body))
+  const codewarsRank = body.ranks.languages.javascript.rank;
   return Math.abs(codewarsRank);
 };
 
-const getCodewars = () => {
+const getCodewars = (username) => {
+  options.uri += 'astroash';
   return rp(options)
     .then(getKyu)
     .catch((err) => {
--- tests/codewars-api.test.js
+++ tests/codewars-api.test.js
@@ -1,7 +1,12 @@
 const tape = require("tape");
 const { getKyu, getCodewars } = require('../model/codewars-api');
 const codewarsSuccessData = require("./dummy-data/codewars-response-success.json");
+const nock = require('nock');
+const path = require('path');
+const supertest = require('supertest');
+const app = require('../app');
 //const codewarsFailData = require("./dummy-data/codewars-response-fail.json");
+
 
 tape('Codewars API: getKyu', (t) => {
   const actual = typeof getKyu(codewarsSuccessData);
@@ -9,8 +14,13 @@ tape('Codewars API: getKyu', (t) => {
   t.end();
 });
 
-//tape('Codewars API: getCodewars for unknown user', (t) => {
-  //const actual = getCodewars(codewarsFailData).statusCode;
-  //t.equal(actual, 404, 'Unknown user will return a 404 error code');
-  //t.end();
-//});
+tape('Codewars API: getCodewars', (t) => {
+  nock('https://www.codewars.com/') 
+  .get('/api/v1/users/astroash')
+  .replyWithFile(200, path.join(__dirname, 'dummy-data', 'codewars-response-success.json'));
+  getCodewars('astroash')
+  .then((kyu) => {
+    t.equal(kyu, 5, 'getCodewars for valid username returns correct kyu (5)');
+    t.end();
+  });
+});
