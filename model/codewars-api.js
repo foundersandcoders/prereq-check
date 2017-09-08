9b2ca2f556598af5c3b3592c8320817694b45954
Dakshina Scott <dakshina.scott@gmail.com>
2017-09-08 16:00:29 +0000

    #2 Tests invalid username and render result

--- model/codewars-api.js
+++ model/codewars-api.js
@@ -1,23 +1,21 @@
 const request = require('request');
 const rp = require('request-promise-native')
 
-const options = {
-  uri: 'https://www.codewars.com/api/v1/users/',
-  json: true, // Automatically parses the JSON string in the response
-};
-
 const getKyu = (body) => {
-  console.log(JSON.stringify(body))
   const codewarsRank = body.ranks.languages.javascript.rank;
   return Math.abs(codewarsRank);
 };
 
 const getCodewars = (username) => {
-  options.uri += 'astroash';
+  const options = {
+    uri: 'https://www.codewars.com/api/v1/users/',
+    json: true, // Automatically parses the JSON string in the response
+  };
+  options.uri += username;
   return rp(options)
     .then(getKyu)
     .catch((err) => {
-      throw err;
+      return err;
     });
 };
 
--- tests/codewars-api.test.js
+++ tests/codewars-api.test.js
@@ -3,10 +3,6 @@ const { getKyu, getCodewars } = require('../model/codewars-api');
 const codewarsSuccessData = require("./dummy-data/codewars-response-success.json");
 const nock = require('nock');
 const path = require('path');
-const supertest = require('supertest');
-const app = require('../app');
-//const codewarsFailData = require("./dummy-data/codewars-response-fail.json");
-
 
 tape('Codewars API: getKyu', (t) => {
   const actual = typeof getKyu(codewarsSuccessData);
@@ -14,13 +10,27 @@ tape('Codewars API: getKyu', (t) => {
   t.end();
 });
 
-tape('Codewars API: getCodewars', (t) => {
+tape('Codewars API: getCodewars valid username', (t) => {
   nock('https://www.codewars.com/') 
   .get('/api/v1/users/astroash')
   .replyWithFile(200, path.join(__dirname, 'dummy-data', 'codewars-response-success.json'));
   getCodewars('astroash')
   .then((kyu) => {
     t.equal(kyu, 5, 'getCodewars for valid username returns correct kyu (5)');
+    t.end();
+  });
+});
+
+tape('Codewars API: getCodewars invalid username', (t) => {
+  const username = 'astroashaaa';
+  nock('https://www.codewars.com/') 
+  .get('/api/v1/users/' + username)
+  .replyWithFile(404, path.join(__dirname, 'dummy-data', 'codewars-response-fail.json'));
+  getCodewars(username)
+  .then((response) => {
+    t.equal(response.toString(),
+    'StatusCodeError: 404 - "{ success: false, reason: \'not found\' }"',
+    'Returns 404 error');
     t.end();
   });
 });
