df3b0eacac92dbd5b018aa81b1bec3c9f9b002d1
astroash <astroaisha@gmail.com>
2017-09-11 19:56:01 +0100

    #6 Made handler, grab handles from webpage

---
+++ routes/scrape-links.js
@@ -0,0 +1,40 @@
+const rp = require('request-promise-native');
+const normalizeUrl = require('normalize-url');
+
+const getGithubLink = (htmlString) => {
+  const regEx = /github.com\/([\w-]*)/;
+  return regEx.exec(htmlString)[1];
+};
+
+const getFccLink = (htmlString) => {
+  const regEx = /freecodecamp.(org|com)\/([\w-]*)/;
+  return regEx.exec(htmlString)[2];
+};
+
+const getCodewarsLink = (htmlString) => {
+  const regEx = /codewars.com\/users\/([\w-]*)/;
+  return regEx.exec(htmlString)[1];
+};
+
+const scrapeLinks = (req, res) => {
+  const url = normalizeUrl(req.query.githubPage);
+  return rp(url)
+    .then((htmlString) => {
+      const githubScrape = {
+        gitHubHandle: getGithubLink(htmlString),
+        fccHandle: getFccLink(htmlString),
+        codewarsHandle: getCodewarsLink(htmlString),
+      };
+      res.render('links', githubScrape)
+    })
+    .catch((err) => {
+      console.error('Fetching FreeCodeCamp crawl failed');
+      console.error(err);
+      const githubScrape = {
+        success: false,
+        message:'Page not found',
+      };
+      res.render('links', githubScrape)
+    });
+};
+module.exports = scrapeLinks;
