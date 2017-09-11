a41f8451576174bee101fa5de73c986001c5d0a6
Dakshina Scott <dakshina.scott@gmail.com>
2017-09-11 11:58:00 +0000

    #5 getGithubPage returns object

--- model/github-page.js
+++ model/github-page.js
@@ -6,14 +6,25 @@ const getGithubPage = (url) => {
     resolveWithFullResponse: true,
   };
   return rp(options)
-  .then((response) => {
-    return response.statusCode === 200;
-  })
-  .catch((err) => {
-    console.error('Fetching GitHub page failed');
-    console.error(err);
-    return err;
-  });
+    .then((response) => {
+      const githubObj = {};
+      githubObj.success = true;
+      return githubObj;
+    })
+    .catch((err) => {
+      console.error('Fetching GitHub page failed');
+      // console.error(err);
+      const githubObj = {};
+      githubObj.success = false;
+      githubObj.statusCode = err.statusCode;
+      if (err.statusCode === 404) {
+        githubObj.message = 'Page not found';
+      }
+      else {
+        githubObj.message = 'Error retrieving page';
+      }
+      return githubObj;
+    });
 };
 
 module.exports = { getGithubPage };
