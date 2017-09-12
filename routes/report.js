0eae1fba204d3034b87705ca8509a385e4994f23
Bart Bucknill <bartbucknill@hotmail.com>
2017-09-12 19:54:41 +0100

    #7 Get number of repos from github and test

--- routes/report.js
+++ routes/report.js
@@ -5,17 +5,20 @@ const { getCodewars } = require("../model/codewars-api");
 const { getFreeCodeCamp } = require("../model/freecodecamp-crawl");
 const { getGithubPage } = require('../model/github-page');
 const { getW3Validator } = require('../model/w3-validator');
+const { getGithubRepos } = require('../model/github-repo-api');
 
 const displayReport = (req, res) => {
-  Promise.all([getCodewars('astroash'), getFreeCodeCamp('astroash'), getGithubPage('http://www.astroash.com/'), getW3Validator('http://www.astroash.com/')])
+  //args to promises to be grabbed from request object
+  Promise.all([getCodewars('astroash'), getFreeCodeCamp('astroash'), getGithubPage('http://www.astroash.com/'), getW3Validator('http://www.astroash.com/'), getGithubRepos('astroash')])
     .then((values) => {
       const summaryObject = {};
       summaryObject.codewars = values[0];
       summaryObject.freeCodeCamp = values[1];
       summaryObject.githubPage = values[2];
       summaryObject.w3Validation = values[3];
-      summaryObject.githubHandle = 'astroash';
-      console.log(typeof summaryObject.githubHandle);
+      summaryObject.githubRepos = values[4];
+      summaryObject.githubHandle = 'astroash'; //need to grab from request object
+      console.log(summaryObject);
 
       res.render('report', summaryObject);
     });
