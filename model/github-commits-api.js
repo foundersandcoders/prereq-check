b5f22ba3700dead245f27e15100b83714266f923
Bart Bucknill <bartbucknill@hotmail.com>
2017-09-12 21:22:07 +0100

    #7 Test github commits module

---
+++ model/github-commits-api.js
@@ -0,0 +1,39 @@
+const rp = require('request-promise-native');
+
+const getRepoName = (url) => {
+  const newUrl = url.replace('https://', '');
+  const reg = /[\w.]+\/([\w-]+)/;
+  const repo = reg.exec(url);
+  return repo ? repo[1] : newUrl;
+};
+
+const getGithubCommits = (githubHandle, url) => {
+  const repo = getRepoName(url);
+  const options = {
+    uri: `https://api.github.com/repos/${githubHandle}/${repo}/commits`,
+    headers: {
+      'User-Agent': 'prereq-check',
+    },
+    json: true, // Automatically parses the JSON string in the response
+  };
+  return rp(options)
+    .then((apiRes) => {
+      return {
+        success: true,
+        repos: apiRes.length,
+      };
+    })
+    .catch((err) => {
+      console.error('Get Github commit count failed');
+      console.error(err);
+      return {
+        success: false,
+        message: 'Fetching Github commit count failed',
+      };
+    });
+};
+
+module.exports = {
+  getRepoName,
+  getGithubCommits,
+};
--- routes/report.js
+++ routes/report.js
@@ -6,10 +6,11 @@ const { getFreeCodeCamp } = require("../model/freecodecamp-crawl");
 const { getGithubPage } = require('../model/github-page');
 const { getW3Validator } = require('../model/w3-validator');
 const { getGithubRepos } = require('../model/github-repo-api');
+const { getGithubCommits } = require('../model/github-commits-api');
 
 const displayReport = (req, res) => {
   //args to promises to be grabbed from request object
-  Promise.all([getCodewars('astroash'), getFreeCodeCamp('astroash'), getGithubPage('http://www.astroash.com/'), getW3Validator('http://www.astroash.com/'), getGithubRepos('astroash')])
+  Promise.all([getCodewars('astroash'), getFreeCodeCamp('astroash'), getGithubPage('http://www.astroash.com/'), getW3Validator('http://www.astroash.com/'), getGithubRepos('astroash'), getGithubCommits('bartbucknill', 'https://bartbucknill.github.io/fac-application/')])
     .then((values) => {
       const summaryObject = {};
       summaryObject.codewars = values[0];
@@ -17,6 +18,7 @@ const displayReport = (req, res) => {
       summaryObject.githubPage = values[2];
       summaryObject.w3Validation = values[3];
       summaryObject.githubRepos = values[4];
+      summaryObject.githubCommits = values[5];
       summaryObject.githubHandle = 'astroash'; //need to grab from request object
       console.log(summaryObject);
 
