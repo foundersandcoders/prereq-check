8392c43d5bc748f690bef58056c5fc107d2af02a
astroash <astroaisha@gmail.com>
2017-09-12 10:51:42 +0100

    #11 Refactor hbs logic :sparkles:

--- routes/report.js
+++ routes/report.js
@@ -6,7 +6,7 @@ const { getFreeCodeCamp } = require("../model/freecodecamp-crawl");
 const { getGithubPage } = require('../model/github-page');
 
 const displayReport = (req, res) => {
-  Promise.all([getCodewars('astroash'), getFreeCodeCamp('fcce3abbd74-b40e-4e5d-96a8-c7e1992dcfe1'), getGithubPage('http://www.astroash.com/')])
+  Promise.all([getCodewars('astroash'), getFreeCodeCamp('astroashaaaa'), getGithubPage('http://www.astroash.com/')])
     .then((values) => {
       const summaryObject = {};
       summaryObject.codewars = values[0];
