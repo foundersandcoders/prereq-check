d7cd86bfae960ef0bbcdf336f05f69d43b815310
Dakshina Scott <dakshina.scott@gmail.com>
2017-09-13 13:59:29 +0000

    #45 Return null if no handle is found :bug:

--- routes/scrape-links.js
+++ routes/scrape-links.js
@@ -1,19 +1,23 @@
 const rp = require('request-promise-native');
 const normalizeUrl = require('normalize-url');
 
+
 const getGithubLink = (htmlString) => {
   const regEx = /github.com\/([\w-]*)/;
-  return regEx.exec(htmlString)[1];
+  const ghHandle = regEx.exec(htmlString);
+  return ghHandle ? ghHandle[1] : null;
 };
 
 const getFccLink = (htmlString) => {
   const regEx = /freecodecamp.(org|com)\/([\w-]*)/;
-  return regEx.exec(htmlString)[2];
+  const fccHandle = regEx.exec(htmlString);
+  return fccHandle ? fccHandle[2] : null;
 };
 
 const getCodewarsLink = (htmlString) => {
   const regEx = /codewars.com\/users\/([\w-]*)/;
-  return regEx.exec(htmlString)[1];
+  const cwHandle = regEx.exec(htmlString);
+  return cwHandle ? cwHandle[1] : null;
 };
 
 const scrapeLinks = (req, res) => {
@@ -31,7 +35,7 @@ const scrapeLinks = (req, res) => {
     })
     .catch((err) => {
       console.error('Fetching Github Pages URL failed');
-      // console.error(err);
+      console.error(err);
       const githubScrape = {
         success: false,
         message:'Page not found',
