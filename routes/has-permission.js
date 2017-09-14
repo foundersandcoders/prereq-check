3299da9fc754af2c31bf1b6e4875756a41b53ad7
Bart Bucknill <bartbucknill@hotmail.com>
2017-09-14 20:26:24 +0100

    #8 only allow users with cookie to access restricted roots

--- routes/has-permission.js
+++ routes/has-permission.js
@@ -1,6 +1,9 @@
 const adminUsers = require('../admins.json');
 
 module.exports = (req, res) => {
+  if (!req.session.user) {
+    return false;
+  }
   const isAdmin = adminUsers.find( (adminUser) => {
     return adminUser.toLowerCase() === req.session.user.toLowerCase();
   })
