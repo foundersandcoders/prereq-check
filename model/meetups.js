a6f89dc8a9e46bfbbb73d220db4d87f35361e399
Dakshina Scott <dakshina.scott@gmail.com>
2017-09-15 13:27:17 +0100

    #10 Turn callbacks into promises

--- model/meetups.js
+++ model/meetups.js
@@ -1,51 +1,57 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
-const async = require('async');
 require('env2')('config.json');
 
-const getMeetupCount = githubHandle => new Promise((resolve, reject) => {
+const getMeetupCount = githubHandle => {
   const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_KEY);
-  let sheet;
-  async.series(
-    [
-      function setAuth(step) {
-        const creds_json = {
-          client_email: process.env.CLIENT_EMAIL,
-          private_key: process.env.PRIVATE_KEY,
-        };
-        doc.useServiceAccountAuth(creds_json, step);
-      },
-      function getInfoAndWorksheets(step) {
-        doc.getInfo((err, info) => {
-          sheet = info.worksheets[0];
-          step();
-        });
-      },
-      function workingWithRows(step) {
-        sheet.getRows(
-          {
-            query: `githubnameunique=${githubHandle}`,
-          },
-          (err, rows) => {
-            resolve({
-              success: true,
-              count: rows[0] ? rows[0].countunique : 0,
-              ghHandle: githubHandle,
-            });
-          },
-        );
-      },
-    ],
-    (err) => {
-      if (err) {
-        console.error('Error retrieving meetup data');
-        // console.error(err);
-        reject({
-          success: false,
-          message: 'Error retrieving meetup data',
-        });
-      }
-    },
-  );
-});
+
+  const setAuth= new Promise((resolve, reject) => {
+    const credsJson = {
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY,
+    };
+    doc.useServiceAccountAuth(credsJson, (err) => {
+      if (err) reject(Error('Couldnt set authentication'));
+      resolve();
+    });
+  });
+
+  const getWorksheet = () => {
+    return new Promise((resolve, reject) => {
+      doc.getInfo((err, info) => {
+        if (err) reject(Error('Couldnt get worksheet'));
+        resolve(info.worksheets[0]);
+      });
+    });
+  };
+
+  const getAttendance = (sheet) => {
+    return new Promise((resolve, reject) => {
+      // query google sheets api to get row where github handle matches 'githubHandle' argument
+      // githubnameunique is the column name in google sheets
+      sheet.getRows(
+        {
+          query: `githubnameunique=${githubHandle}`,
+        },
+        (err, rows) => {
+          if (err) reject(Error('Couldnt get rows'));
+          resolve({
+            success: true,
+            // countunique is the name of the column in google sheets
+            count: rows[0] ? rows[0].countunique : 0,
+            ghHandle: githubHandle,
+          });
+        },
+      );
+    });
+  };
+
+  return setAuth
+    .then(getWorksheet)
+    .then(getAttendance)
+    .catch((err) => {
+      console.error(err);
+      return err;
+    });
+};
 
 module.exports = getMeetupCount;
