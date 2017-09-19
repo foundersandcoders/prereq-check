a29f171dbab4db7ea487a63df09d9c5d9906d9b6
astroash <astroaisha@gmail.com>
2017-09-19 22:42:29 +0100

    #71 Refactor validation functions into 1 function refactor object assignment create test for getFccScore :robot:

--- model/freecodecamp-crawl.js
+++ model/freecodecamp-crawl.js
@@ -1,46 +1,40 @@
 const rp = require('request-promise-native');
 
-const fccArrays = require('./freecodecamp-arrays');
+const {
+  htmlCss,
+  basicJavaScript,
+  oOFunctionalProgramming,
+  basicScripting,
+} = require('./freecodecamp-arrays');
 
-const htmlCssValidator = (htmlString) => {
-  return fccArrays.htmlCss.every(element => htmlString.indexOf(element) !== -1);
-};
-
-const basicJavaScriptValidator = (htmlString) => {
-  return fccArrays.basicJavaScript.every(element => htmlString.indexOf(element) !== -1);
-};
-
-const oOFunctionalProgrammingValidator = (htmlString) => {
-  return fccArrays.oOFunctionalProgramming.every(element => htmlString.indexOf(element) !== -1);
-};
-
-const basicScriptingValidator = (htmlString) => {
-  return fccArrays.basicScripting.every(element => htmlString.indexOf(element) !== -1);
-};
+const fccSectionValidator = (htmlString, fccArray) => fccArray.every(element =>
+  htmlString.indexOf(element) !== -1);
 
 const getFccScore = (htmlString) => {
-  const regEx = /text-primary\">\[ (\d+)/;
+  const regEx = /text-primary">\[ (\d+)/;
   return regEx.exec(htmlString)[1];
 };
 
 const getFreeCodeCamp = (username) => {
   const options = {
     uri: `https://www.freecodecamp.org/${username}`,
-  }
+  };
   return rp(options)
     .then((htmlString) => {
       const reg = new RegExp(username, 'gi');
       if (!htmlString.match(reg)) {
         throw Error('User not found');
       } else {
-        const freeCodeCampObj = {};
-        freeCodeCampObj.success = true;
-        freeCodeCampObj.score = getFccScore(htmlString);
-        freeCodeCampObj.htmlCss = htmlCssValidator(htmlString);
-        freeCodeCampObj.basicJavaScript = basicJavaScriptValidator(htmlString);
-        freeCodeCampObj.oOFunctionalProgramming = oOFunctionalProgrammingValidator(htmlString);
-        freeCodeCampObj.basicScripting = basicScriptingValidator(htmlString);
-        freeCodeCampObj.complete = freeCodeCampObj.htmlCss && freeCodeCampObj.basicJavaScript && freeCodeCampObj.oOFunctionalProgramming && freeCodeCampObj.basicScripting;
+        const freeCodeCampObj = {
+          success: true,
+          score: getFccScore(htmlString),
+          htmlCss: fccSectionValidator(htmlString, htmlCss),
+          basicJavaScript: fccSectionValidator(htmlString, basicJavaScript),
+          oOFunctionalProgramming: fccSectionValidator(htmlString, oOFunctionalProgramming),
+          basicScripting: fccSectionValidator(htmlString, basicScripting),
+        };
+        freeCodeCampObj.complete = freeCodeCampObj.htmlCss && freeCodeCampObj.basicJavaScript
+          && freeCodeCampObj.oOFunctionalProgramming && freeCodeCampObj.basicScripting;
         return freeCodeCampObj;
       }
     })
@@ -55,9 +49,7 @@ const getFreeCodeCamp = (username) => {
 };
 
 module.exports = {
-  htmlCssValidator,
-  basicJavaScriptValidator,
-  oOFunctionalProgrammingValidator,
-  basicScriptingValidator,
+  fccSectionValidator,
   getFreeCodeCamp,
+  getFccScore,
 };
